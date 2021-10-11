7fef66244f5652d7546c14caea3a15810b71da27
ClaimAcademyMentor <65732625+ClaimAcademyMentor@users.noreply.github.com>
2021-10-11 11:15:11 -0500

    Update
    Update to the project

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("This is a project test, using it to initial commit and push a project to GitHub");
+		System.out.println("This is an update to the project");
 	}
 
 }
